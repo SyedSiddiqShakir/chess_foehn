6aa9698c7fa090e1c4b9d04385f8ea7e6bf46b37
SyedSiddiqShakir <syedsiddiqshakir@gmail.com>
2025-10-03 21:31:03 +0200

    parsing multi-PGN files

--- mess_around.py
+++ mess_around.py
@@ -1,5 +1,22 @@
+import chess
 
+with open('chess_games_data\latestgame.pgn', 'r', encoding='utf-8') as myfile1:
+    content1 = myfile1.read()
+with open('chess_games_data\latestgame2.pgn', 'r', encoding='utf-8') as myfile2:
+    content2 = myfile2.read()
+with open('chess_games_data\latestgame3.pgn', 'r', encoding='utf-8') as myfile3:
+    content3 = myfile3.read()
 
-with open('chess_games_data\lichess_elite_2013-09.pgn', 'r', encoding='utf-8') as myfile:
-    content = myfile.read()
-print(content)
+def strip_lines(content):
+    lines = content.strip().split('\n')
+    return lines
+
+def remove_headers(lines):
+    moves = []
+    for line in lines:
+        if not line.startswith('['):
+            moves.append(line)
+    moves = list(filter(None, moves))
+    return moves
+
+print(remove_headers(strip_lines(content1)))
